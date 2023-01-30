418b3ee042e40f60035be1fc6e61de09abc04c37
Jessica <jesscara.993@gmail.com>
2023-01-30 18:28:37 +0100

    structure blade

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+  <div class="container d-flex justify-content-center align-items-center">
+    <a href="#">
+      <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="DC">
+    </a>
+
+    <nav>
+      <ul class="m-0">
+        <li>CHARACTERS</li>
+        <li class="active">COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </nav>
+  </div>
+
+  <div class="jumbo">
+    <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+  </div>
+</header>
