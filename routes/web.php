f96c86b19de1ff8f42038a2e6a8f69bc9b04fe82
Jessica <jesscara.993@gmail.com>
2023-01-30 16:32:43 +0100

    pages connected

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-  return view('welcome');
+  return view('comics-cards');
+});
+
+Route::get('/', function () {
+  return view('single-comics');
 });
