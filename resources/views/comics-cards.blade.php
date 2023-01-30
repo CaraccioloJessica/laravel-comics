bfdb39c33d6371b9eb551c9a0e34130eb0197d6b
Jessica <jesscara.993@gmail.com>
2023-01-30 17:17:12 +0100

    name change

--- resources/views/comics-cards.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.main-layout')
-
-@section('head')
-  <title>Comics cards - page 1</title>    
-@endsection
-
-@section('content')
-  <h1><a href="single-comics"> Hello </a></h1>
-@endsection
--- resources/views/single-comics.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.main-layout')
-
-@section('head')
-  <title>Single Comics - page 2</title>    
-@endsection
-
-@section('content')
-  <h2>Seconda pagina</h2>
-@endsection
